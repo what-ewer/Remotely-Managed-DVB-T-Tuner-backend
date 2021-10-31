cd3883ec1c29d87c21158528a1d3dadd6793d88e
Karol Koptyra <koptyrakarol@gmail.com>
2021-10-31 23:05:21 +0100

    Change recorded api so it updates record information

--- src/api/recorded.py
+++ src/api/recorded.py
@@ -21,14 +21,42 @@ class RecordedAPI:
 
     def post_recorded(self, id, recorded):
         for o in recorded:
-            try:
-                query = f"""INSERT INTO recorded_files(tuner_id, channel_id, program_name, record_size, start, end) \
-                    VALUES({id}, '{o.channel_id}', '{o.program_name}', {o.record_size}, {o.start}, {o.end})"""
-            except:
-                return Response("Wrong record recorded list", status=400)
-            else:
+            if self.__order_with_id_exists(o.order_id):
                 try:
-                    self.db_manager.execute_query(query)
-                except Exception as exc:
-                    return Response(str(exc), status=500)
+                    query = f"""INSERT INTO recorded_files(order_id, tuner_id, channel_id, program_name, record_size, start, end) \
+                        VALUES({o.order_id}, {id}, '{o.channel_id}', '{o.program_name}', {o.record_size}, {o.start}, {o.end})"""
+                except:
+                    return Response("Wrong record recorded list", status=400)
+                else:
+                    try:
+                        self.db_manager.execute_query(query)
+                        self.__update_information(o.order_id, o.record_size, o.file_name)
+                    except Exception as exc:
+                        return Response(str(exc), status=500)
+            else:
+                return Response("order with that id doesnt exist", status=400)
         return Response("successfully posted recorded", status=200)
+
+    def __update_information(self, order_id, record_size, filename):
+        query = f"""UPDATE record_information
+            SET record_size = {record_size}, file_name = '{filename}'
+            WHERE order_id = {order_id}"""
+
+        try:
+            self.db_manager.execute_query(query)
+        except Exception as exc:
+            return Response(str(exc), status=500)
+
+        return Response("successfully updated channels", status=200)
+
+    def __order_with_id_exists(self, id):
+        query = f"""SELECT *
+            FROM record_orders
+            WHERE order_id = {id}"""
+
+        try:
+            res = self.db_manager.execute_query(query)
+        except Exception as exc:
+            return False
+
+        return res
