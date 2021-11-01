6d05ff48b990db49c0e07ae71018f73a457caabd
Karol Koptyra <koptyrakarol@gmail.com>
2021-11-01 18:59:07 +0100

    Rewrite settings api to support anti sqli

--- src/api/settings.py
+++ src/api/settings.py
@@ -1,6 +1,8 @@
+from typing import Set
 from flask import Response
 import json, requests
 from src.api import heartbeat
+from src.database.db_model import Settings
 
 
 class SettingsAPI:
@@ -8,29 +10,39 @@ class SettingsAPI:
         self.db_manager = db_manager
 
     def get_settings(self, id):
-        query = f"""SELECT recording_location, tvh_username, tvh_password FROM settings
-            WHERE tuner_id = {id}"""
-        try:
-            settings = self.db_manager.execute_query(query)
-        except Exception as exc:
-            return Response(str(exc), status=500)
+        query = f"""SELECT recording_location, tvh_username, tvh_password 
+            FROM settings
+            WHERE tuner_id = ?"""
+        args = [id]
 
-        result = [
-            {"recording_location": s[0], "tvh_username": s[1], "tvh_password": s[2]}
-            for s in settings
-        ]
-        return Response(json.dumps(result), status=200, mimetype="json")
+        result = self.db_manager.run_query(query, args)
+        if result:
+            settings = Settings(*result[0])
+            return Response(
+                json.dumps(settings, default=lambda o: o.__dict__, indent=4),
+                mimetype="json",
+                status=200,
+            )
+        else:
+            return Response("Something went wrong", status=500)
 
     def post_settings(self, id, username, password, settings):
-        try:
-            query = f"""INSERT OR REPLACE INTO settings(tuner_id, recording_location, tvh_username, tvh_password)
-                        VALUES({id}, '{settings.recording_location}', '{settings.tvh_username}', '{settings.tvh_password}')"""
-        except:
-            return Response("Wrong settings list", status=400)
+        query = f"""INSERT OR REPLACE INTO settings(tuner_id, recording_location, tvh_username, tvh_password)
+                        VALUES(?, ?, ?, ?)"""
+        args = [
+            id,
+            settings.recording_location,
+            settings.tvh_username,
+            settings.tvh_password,
+        ]
+
+        if self.db_manager.run_query(query, args, return_result=False):
+            # POST that we provided needed Settings information
+            requests.post(
+                url=f"{heartbeat.url}/ask",
+                params={"id": id, "information": "changed_settings"},
+                auth=(username, password),
+            )
+            return Response("Successfully updated Settings", status=201)
         else:
-            try:
-                requests.post(url=f"{heartbeat.url}/ask", params={"id": id, "information": "changed_settings"}, auth=(username, password))
-                self.db_manager.execute_query(query)
-            except Exception as exc:
-                return Response(str(exc), status=500)
-        return Response("successfully posted settings", status=200)
+            return Response("Something went wrong", status=500)
