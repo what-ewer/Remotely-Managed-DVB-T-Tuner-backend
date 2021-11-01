bc9a064b9fb279edeb6368316989f4925d68f923
Karol Koptyra <koptyrakarol@gmail.com>
2021-11-01 21:44:51 +0100

    Rewrite auth to support anti sqli

--- src/auth/auth.py
+++ src/auth/auth.py
@@ -10,104 +10,85 @@ class UserAuth:
         (user_id, val) = self.__verify_user(username, password)
         return val and self.__verify_id(id, user_id)
 
-    def __verify_user(self, username, password):
-        try:
-            query = f"""SELECT id, login, password FROM users
-                WHERE login = '{username}' AND password = '{password}'"""
-        except Exception as exc:
-            return (-1, False)
+    def register(self, username, password):
+        if self.__user_already_exists(username):
+            return Response(
+                json.dumps(
+                    {
+                        "status": False,
+                        "text": f"User with username {username} already exists",
+                    }
+                ),
+                status=400,
+            )
+        elif self.__register_user(username, password):
+            return Response(
+                json.dumps(
+                    {"status": True, "text": f"Successfully registered {username}"}
+                ),
+                status=201,
+            )
         else:
-            try:
-                user = self.db_manager.execute_query(query)
-            except Exception as exc:
-                return (-1, False)
-        return (user[0][0], True) if user else (-1, False)
-
-    def __verify_id(self, id, user_id):
-        try:
-            query = f"""SELECT user_id, tuner_id
-                WHERE user_id = '{user_id}' AND tuner_id = '{id}'"""
-        except Exception as exc:
-            return (-1, False)
-        else:
-            try:
-                user_tuner = self.db_manager.execute_query(query)
-            except Exception as exc:
-                return (-1, False)
-        return user_tuner
+            return Response(
+                json.dumps(f"Something went wrong while registering"), status=500
+            )
 
     def check_login(self, user, password):
         id = self.__get_user_id(user, password)
-        if not id:
-            return Response(json.dumps({"status": False, "tuner_ids": []}), status=200)
-        else:
+        if id:
             return Response(
                 json.dumps({"status": True, "tuner_ids": self.__get_user_tuners(id)}),
                 status=200,
             )
+        else:
+            return Response(json.dumps({"status": False, "tuner_ids": []}), status=400)
+
+    def __verify_user(self, username, password):
+        query = """SELECT id, login, password 
+            FROM users
+            WHERE login = ? AND password = ?"""
+        args = [username, password]
+
+        result = self.db_manager.run_query(query, args)
+        return (result[0][0], True) if result else (-1, False)
+
+    def __verify_id(self, id, user_id):
+        query = f"""SELECT user_id, tuner_id
+            WHERE user_id = ? AND tuner_id = ?"""
+        args = [user_id, id]
+
+        result = self.db_manager.run_query(query, args)
+        return result if result else (-1, False)
 
     def __get_user_id(self, user, password):
-        try:
-            query = f"""SELECT id FROM users
-                WHERE login = '{user}' AND password = '{password}'"""
-        except Exception as exc:
-            return False
-        else:
-            try:
-                id = self.db_manager.execute_query(query)
-            except Exception as exc:
-                return False
-        return id[0][0] if id else False
+        query = """SELECT id 
+            FROM users
+            WHERE login = ? AND password = ?"""
+        args = [user, password]
+
+        result = self.db_manager.run_query(query, args)
+        return result[0][0] if result else 0
 
     def __get_user_tuners(self, id):
-        try:
-            query = f"""SELECT tuner_id FROM user_tuners
-                WHERE user_id = {id}"""
-        except Exception as exc:
-            return []
-        else:
-            try:
-                tuner_ids = self.db_manager.execute_query(query)
-            except Exception as exc:
-                return []
-        return [i[0] for i in tuner_ids] if tuner_ids else []
+        query = """SELECT tuner_id 
+            FROM user_tuners
+            WHERE user_id = ?"""
+        args = [id]
 
-    def register(self, username, password):
-        if self.__user_already_exists(username):
-            return Response(
-                json.dumps({"status": False, "text": "User with username {username} already exists"}), status=200
-            )
-        elif self.__register_user(username, password):
-            return Response(
-                json.dumps({"status": True, "text": "Successfully registered {username}"}), status=200
-            )
-        else:
-            return Response(
-                json.dumps(f"Something went wrong while registering"), status=400
-            )
+        result = self.db_manager.run_query(query, args)
+        return [r[0] for r in result] if result else []
 
     def __user_already_exists(self, username):
-        try:
-            query = f"""SELECT id FROM users
-                WHERE login = '{username}'"""
-        except Exception as exc:
-            return True
-        else:
-            try:
-                exists = self.db_manager.execute_query(query)
-            except Exception as exc:
-                return True
-        return exists
+        query = """SELECT id 
+            FROM users
+            WHERE login = ?"""
+        args = [username]
+
+        return self.db_manager.run_query(query, args)
 
     def __register_user(self, username, password):
-        try:
-            query = f"""INSERT INTO users (login, password)
-                VALUES ('{username}', '{password}')"""
-        except Exception as exc:
-            return False
-        else:
-            try:
-                registered = self.db_manager.execute_query(query)
-            except Exception as exc:
-                return False
-        return True
+        query = f"""INSERT INTO users (login, password)
+                VALUES (?, ?)"""
+        args = [username, password]
+
+        return self.db_manager.run_query(query, args, return_result=False)
